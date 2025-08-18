8652a1aa751ef9cea493750950885a73c4b8d177
jeetsil07 <jeetsil100@gmail.com>
2025-08-18 08:37:01 +0530

    Initial commit

---
+++ hibernatebasic/src/main/java/com/jeet/hibernatebasic/HibernatebasicApplication.java
@@ -0,0 +1,13 @@
+package com.jeet.hibernatebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatebasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatebasicApplication.class, args);
+	}
+
+}
---
+++ hibernatebasic/src/test/java/com/jeet/hibernatebasic/HibernatebasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeet.hibernatebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatebasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
